c1f7c68a5d13bd3a3eb17f052106d5e95ed27e89
An Vuong <an.vuong@gooddata.com>
2020-10-27 13:45:32 +0700

    Improve performance of transformation of header items in an AFM execution result
    JIRA: SD-1212

--- libs/sdk-backend-bear/src/convertors/dateFormatting/dateFormatter.ts
+++ libs/sdk-backend-bear/src/convertors/dateFormatting/dateFormatter.ts
@@ -1,11 +1,47 @@
 // (C) 2020 GoodData Corporation
-import { IResultHeader, isResultAttributeHeader } from "@gooddata/sdk-backend-spi";
+import {
+    IAttributeDescriptor,
+    IDimensionDescriptor,
+    IResultHeader,
+    isAttributeDescriptor,
+    isResultAttributeHeader,
+} from "@gooddata/sdk-backend-spi";
 import { createDateValueFormatter } from "./dateValueFormatter";
-import { DateFormat } from "./dateValueParser";
+import { DateFormat, DEFAULT_DATE_FORMAT } from "./dateValueParser";
 import { createDefaultDateFormatter } from "./defaultDateFormatter";
 
-export function transformDateFormat(resultHeader: IResultHeader, dateFormat?: DateFormat): IResultHeader {
-    if (!isResultAttributeHeader(resultHeader) || !dateFormat) {
+export function findDateAttributeUri(dimensions: IDimensionDescriptor[]): string | undefined {
+    const dateAttrDescriptor = dimensions
+        .reduce((attrDimensions: IAttributeDescriptor[], dimensionItem: IDimensionDescriptor) => {
+            attrDimensions.push(...dimensionItem.headers.filter(isAttributeDescriptor));
+            return attrDimensions;
+        }, [])
+        .find(
+            (attrDescriptor: IAttributeDescriptor): boolean =>
+                attrDescriptor.attributeHeader.identifier.endsWith("mmddyyyy") ||
+                attrDescriptor.attributeHeader.identifier.endsWith("mm_dd_yyyy"),
+        );
+    if (!dateAttrDescriptor) {
+        return undefined;
+    }
+    return dateAttrDescriptor.attributeHeader.formOf.uri;
+}
+
+export function transformDateFormat(
+    resultHeader: IResultHeader,
+    dateAttributeUri?: string,
+    dateFormat?: DateFormat,
+): IResultHeader {
+    if (
+        !isResultAttributeHeader(resultHeader) ||
+        !dateAttributeUri ||
+        !dateFormat ||
+        dateFormat === DEFAULT_DATE_FORMAT
+    ) {
+        return resultHeader;
+    }
+    const resultHeaderUri = resultHeader.attributeHeaderItem.uri;
+    if (!resultHeaderUri.startsWith(dateAttributeUri)) {
         return resultHeader;
     }
     try {
@@ -13,7 +49,7 @@ export function transformDateFormat(resultHeader: IResultHeader, dateFormat?: Da
         return {
             attributeHeaderItem: {
                 name: dateValueFormatter(resultHeader.attributeHeaderItem.name),
-                uri: resultHeader.attributeHeaderItem.uri,
+                uri: resultHeaderUri,
             },
         };
     } catch {
